52cef775790e916d0659d7fc3ef642cbee0400b2
Fouad <laghaahmedfouad@gmail.com>
2023-01-26 10:51:18 +0100

    first commit

---
+++ Compilateur/bin/resultat/PASCAL_programme.pas
@@ -0,0 +1,23 @@
+program  All;
+Var :
+a,b : integer ;
+c : char ;
+h,k : real ;
+o : boolean ;
+begin
+a:=10*5+3;
+repeat
+if (a > 2 and a < 55) do 
+ for ( integer i := 1;i < 10;i++)
+Write ("Tp compilation");
+end;
+else
+b:=5;
+Read (c);while ( b <> c ) do
+begina:=5;
+end;
+end;
+
+until ( b <> c );
+
+end.
---
+++ Compilateur/src/resultat/C_programme.c
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main(){
+ int a,b;
+char c;
+float h,k;
+bool o;
+a = 10*5+3;
+ do { 
+ if (a > 2 && a < 55) { 
+ for ( int i = 1;i < 10;i++) { 
+cout << "Tp compilation" << endl ;
+ }
+
+} else{ 
+b = 5;
+cin >> c >> endl;
+ while ( b != c ) { 
+a = 5;
+ }  }
+ } while ( b != c );
+ 
+return 0; }
